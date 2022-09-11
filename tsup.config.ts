08cb08278bd827413d3b3bdaafc22ea6fb5ef6a0
parbez <imranbarbhuiya.fsd@gmail.com>
2022-09-11 07:16:27 +0530

    chore: change deps

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,21 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
 	clean: true,
-	dts: false,
 	entry: ['src/index.ts'],
 	format: ['esm', 'cjs'],
 	minify: false,
 	skipNodeModulesBundle: true,
 	sourcemap: true,
-	target: 'es2021',
+	target: 'esnext',
 	keepNames: true,
-	tsconfig: 'src/tsconfig.json',
-	esbuildOptions: (options, context) => {
-		if (context.format === 'cjs') {
-			options.banner = {
-				js: '"use strict";'
-			};
-		}
-		options.charset = 'utf8';
-	}
+	tsconfig: 'src/tsconfig.json'
 });
